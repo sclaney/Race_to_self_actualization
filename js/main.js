b6029c129d3b229694b72f824e91b1b951700b85
Stanley Yang <stanley@stanleycyang.com>
2015-07-07 12:31:30 -0700

    Update main.js
    Fixed:
    
    - The function nextPrompt
    - Took out unnecessary argument in updatePrompt

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ var prompts = ['This is the first prompt, it is not meant to really mean anythin
 // Here's the event listener that loads a new prompt when the button is clicked
 // It still needs more logic to delete a prompt once it has been used once.
 // It also needs to only be clickable when a round is over
-button.on('click', nextPrompt {
-  function nextPrompt(prompts){
-    prompts.shift();
-    updatePrompt(prompts[0]);
-  }
-});
+button.on('click', nextPrompt);
+
+function nextPrompt(){  
+  prompts.shift();
+  updatePrompt();
+}
 
 function updatePrompt() {
   prompt.html(prompts[0]);

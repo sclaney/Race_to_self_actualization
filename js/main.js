769630f16e20b8b72d9b3b828fb41c4ecb195d70
Sam Claney <sampclaney@gmail.com>
2015-07-07 16:03:55 -0700

    added the feature to check if the input is the same as the prompt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var prompt = document.getElementById('prompt');
 var response = document.getElementById('response');
 var button = document.getElementById('button');
+var textarea = document.getElementsByTagName('textarea');
 
 // This is the array that holds all the prompts
-var prompts = ['This is the first prompt, it is not meant to really mean anything',
+var prompts = ['This', 'This is the first prompt, it is not meant to really mean anything',
 'This is the second prompt', 'This is the third prompt'];
 
 // Here's the event listener that loads a new prompt when the button is clicked
@@ -11,8 +12,9 @@ var prompts = ['This is the first prompt, it is not meant to really mean anythin
 // It also needs to only be clickable when a round is over
 button.addEventListener('click', nextPrompt);
 
-function nextPrompt(){  
-  prompts.shift();
+function nextPrompt(){
+  var shiftedValue = prompts.shift();
+  prompts.push(shiftedValue);
   updatePrompt();
 }
 
@@ -24,6 +26,27 @@ function updatePrompt() {
 //This will be the logic that states that if the text in the response box is
 // equal to the text in the prompt, the round will end.
 // it should be able to disable the response box so you can't type in it anymore
-if (response.innerHTML === prompt) {
-  prompt.innerHTML = 'You did it!';
-}
+var input = "";
+
+$('textarea').keypress(function() {
+  console.log(String.fromCharCode(event.which));
+  input = input + String.fromCharCode(event.which);
+
+  console.log(input);
+  if(input === $.trim($('#prompt').html())) {
+    alert("You did it!");
+    input = "";
+    return true;
+  }
+});
+
+$('textarea').keydown(function() {
+  if(event.which === 8) { //delete is key code 8
+    input = input.substring(0, input.length -1);
+    console.log(input);
+  }
+});
+
+// $('textarea').keypress(function() {
+// });
+

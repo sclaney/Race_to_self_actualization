38d4742862357fe4c4568ceaa9ecbe828e5c657d
Sam Claney <sampclaney@gmail.com>
2015-07-07 12:21:37 -0700

    trouble updating prompt

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+var prompt = document.getElementById('prompt');
+var response = document.getElementById('response');
+var button = document.getElementById('button');
 
-  console.log('activate framework!');
+// This is the array that holds all the prompts
+var prompts = ['This is the first prompt, it is not meant to really mean anything',
+'This is the second prompt', 'This is the third prompt'];
 
+// Here's the event listener that loads a new prompt when the button is clicked
+// It still needs more logic to delete a prompt once it has been used once.
+// It also needs to only be clickable when a round is over
+button.on('click', nextPrompt {
+  function nextPrompt(prompts){
+    prompts.shift();
+    updatePrompt(prompts[0]);
+  }
+});
+
+function updatePrompt() {
+  prompt.html(prompts[0]);
+}
+
+
+//This will be the logic that states that if the text in the response box is
+// equal to the text in the prompt, the round will end.
+// it should be able to disable the response box so you can't type in it anymore
+if (response.innerHTML === prompt) {
+  prompt.innerHTML = 'You did it!';
+}

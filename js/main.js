775c63bdebf420ebd4e80999e05a96ff78929077
Sam Claney <sampclaney@gmail.com>
2015-07-08 17:16:16 -0700

    added the final win logic, just need to add reset function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var prompt = document.getElementById('prompt');
-var response = document.getElementById('response');
-var button = document.getElementById('button');
-document.getElementById('button').disabled = true;
-var textarea = document.getElementsByTagName('textarea');
+var prompt = $('#prompt');
+var response = $('#response');
+var button = $('#button');
+button.prop('disabled', true);
+var textarea = $('textarea');
 var P1Time; //This variable will hold the time it took player 1 to complete the prompt
 var P2Time; //This variable will hold the time it took player 2 to complete the prompt
-var P1Total; //This variable will keep track of how many rounds player 1 has won
-var P2Total; //This variable will keep track of how many rounds player 2 has won
+var P1Total = 0; //This variable will keep track of how many rounds player 1 has won
+var P2Total = 0; //This variable will keep track of how many rounds player 2 has won
 
 
 /////////////////////////////SWITCH PLAYER//////////////////////////////////////
@@ -21,6 +21,7 @@ function playerSwitch() {
     whichPlayer = 'Player2';
   } else if (whichPlayer === 'Player2') {
     getRoundWinner();
+    getGameWinner();
     whichPlayer = 'Player1';
   }
 }
@@ -32,30 +33,33 @@ function playerSwitch() {
 
 
 // This is the array that holds all the prompts
-var prompts=['This','First prompt','Second prompt','This is the third prompt'];
+var prompts=[' This',' First prompt',' Second prompt',' This is the third prompt'];
 
 // Here's the event listener that loads a new prompt when the button is clicked
 // It still needs more logic to delete a prompt once it has been used once.
 // It also needs to only be clickable when a round is over
-button.addEventListener('click', nextPrompt);
+button.click(nextPrompt);
 
 function nextPrompt(){
   var shiftedValue = prompts.shift();
   prompts.push(shiftedValue);
   updatePrompt();
-  startTimer = new Date().getTime();//Might move this later
+  resetStartTime();
+}
+
+function resetStartTime() {
+  startTimer = new Date().getTime();
 }
 
 function updatePrompt() {
-  prompt.innerHTML = prompts[0];
+  prompt.html(prompts[0]);
   resetStuff();
 }
 
 function resetStuff() {
-  $('textarea').val(' ');
+  textarea.val(' ');
   input = " ";
-  totalTime = " ";
-  document.getElementById('button').disabled = true;
+  button.prop('disabled', true);
 }
 
 /////////////////////////////BUTTON CLICK///////////////////////////////////////
@@ -69,27 +73,29 @@ function resetStuff() {
 // it should be able to disable the response box so you can't type in it anymore
 var input = "";
 
-$('textarea').keypress(function() {
-  console.log(String.fromCharCode(event.which));
-  input = input + String.fromCharCode(event.which);
+textarea.keypress(function() {  //when there is a keypress in the textarea
+  console.log(String.fromCharCode(event.which)); //the console log will show what's pressed
+  input = input + String.fromCharCode(event.which); //and the computer takes that kepress and adds it into input
 
   console.log(input);
-  if(input === $.trim($('#prompt').html())) {
-    var endTimer = new Date().getTime();
-    totalTime = ((endTimer - startTimer)/1000); //polluted the global here by taking away var
-    alert("You did it! It took " + totalTime + " seconds to finish.");
-    document.getElementById('button').disabled = false;
-    playerSwitch();
-    if (whichPlayer === 'Player1') {
-      P1Time = totalTime;
-    } else if (whichPlayer === 'Player2'){
-      P2Time = totalTime;
+  if(input === $.trim(prompt.html())) { //if the users input is equal to the prompt (minus the whitespace)
+    var endTimer = new Date().getTime(); //then the timer stops
+    totalTime = ((endTimer - startTimer)/1000); //and we take the end time and subtract is from start time and divide it by 1000 to get time in seconds
+    alert("You did it! It took " + totalTime + " seconds to finish."); //then an alert tells you you are finished and shows you your time
+    button.prop('disabled', false); //then the button becomes clickable to start the next round
+    if (whichPlayer === 'Player1') { //then we say if the player is player 1
+      P1Time = totalTime; //add this total time to the variable for their time
+      P1Total += 1;
+    } else if (whichPlayer === 'Player2'){ //and if it's player 2
+      P2Time = totalTime; //add the time to the variable that holds player 2's time
+      P2Total += 1;
     }
+      playerSwitch(); //then we switch the player turns WHICH WE DO AFTER THE IF STATEMENT
     return true;
   }
 });
 
-$('textarea').keydown(function() {
+textarea.keydown(function() {
   if(event.which === 8) { //delete is key code 8
     input = input.substring(0, input.length -1);
     console.log(input);
@@ -106,13 +112,29 @@ $('textarea').keydown(function() {
 function getRoundWinner(){
   console.log("Player one's time: " + P1Time);
   console.log("Player two time: " + P2Time);
-  // if (P1Time < P2Time) {
-  //   alert('Player 1 has won this round!');
-  //   P1Total++;
-  // } else if (P2Time < P1Time){
-  //   alert('Player 2 has won this round!');
-  //   P2Total++;
-  // }
+  if (P1Time < P2Time) {
+    alert('Player 1 has won this round!');
+    P1Total++;
+  } else if (P2Time < P1Time){
+    alert('Player 2 has won this round!');
+    P2Total++;
+  }
+}
+
+function getGameWinner() {
+  if (P1Total === 2) {
+    alert('GAME OVER! PLAYER 1 WINS!!!!');
+  } else if (P2Total) {
+    alert('GAME OVER! PLAYER 2 WINS!!!!');
+  }
+}
+
+function resetGame() {
+  textarea.val(' ');
+  input = " ";
+  button.prop('disabled', true);
+  P1Total = 0;
+  P2Total = 0;
 }
 
 /////////////////////////////WIN LOGIC//////////////////////////////////////////
